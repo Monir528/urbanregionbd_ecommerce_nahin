0bbb07f45f3f370f45f6c0bd3d0e81107707cbbb
monir <sagorhossain092@gmail.com>
2025-03-06 05:37:23 +0600

    some changes

---
+++ src/app/(admin)/admin/garbage/page.tsx
@@ -0,0 +1,23 @@
+'use client';
+import {useGetProductsQuery} from "@/components/api/productApi";
+import TableBody from "@/components/TableBody/TableBody";
+import OrderTable from "@/components/OrderTable/OrderTable";
+
+const Overview = () => {
+
+    const {data, isSuccess, isError, isLoading}=useGetProductsQuery()
+    console.log(data);
+
+    return (
+        <main>
+            <div className='mainContent'>
+                <h2>Order Page:</h2>
+                <OrderTable></OrderTable>
+                <p className="h-10"></p>
+            </div>
+        </main>
+
+    );
+};
+
+export default Overview;
